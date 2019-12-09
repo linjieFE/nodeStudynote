6caa6c78276c206b3057a1300ee02c4bb40dd912
zhenlginjie <zhenglinjie789@pingan.com.cn>
2019-12-09 17:33:50 +0800

    nodejs学习笔记

---
+++ app.js
@@ -0,0 +1,10 @@
+// 调试->启动调试->调试控制台
+// 路径 
+console.log(__dirname);//不包含文件名的路径 =>/Users/9t2e20190814/Desktop/nodeDemo
+console.log(__filename);//包含文件名的路径 =>/Users/9t2e20190814/Desktop/nodeDemo/app.js
+
+// global node全局对像 类似 js this
+console.log(global)
+/**打印内容
+ * {DTRACE_NET_SERVER_CONNECTION: , DTRACE_NET_STREAM_END: , DTRACE_HTTP_SERVER_REQUEST: , DTRACE_HTTP_SERVER_RESPONSE: , DTRACE_HTTP_CLIENT_REQUEST: , …}
+ */
