b54bc2a702a494a0a34c422189353997cd464409
zhenlginjie <zhenglinjie789@pingan.com.cn>
2019-12-09 18:15:41 +0800

    三、事件模块 引入 创建 注册 触发

--- REDEME.md
+++ REDEME.md
@@ -1,1 +1,2 @@
 ## nodejs学习笔记
+[视频]（https://ke.qq.com/webcourse/index.html#cid=240103&term_id=100415691&taid=2693507135547879&type=1024&vid=5285890787854110580）
--- app.js
+++ app.js
@@ -14,3 +14,34 @@ console.log(global)
  var stuff =  require("./stuff")
  console.log(stuff.counter(['Jenny','zheng','zheng','jerry']));
  console.log(stuff.adder(1,stuff.pi))
+
+ //三、事件模块
+
+ //3.1 引入事件模块
+ var events = require('events');
+
+//3.2 创建EventEmitter 对象
+
+ const myEmitter =new events.EventEmitter();
+
+ //3.3 注册事件  myEmitter.on('事件名例 click ','回调函数')
+ myEmitter.on('someEvent',function(msg){
+     console.log(msg)//emit中的参数
+ })
+
+ //3.4 触发事件 myEmitter.emit('触发事件名','传的参数')
+ myEmitter.emit('someEvent','实现事件并传递此参数到注册事件的回调函数中')
+ 
+ console.log(1)
+
+ //例二
+
+ myEmitter.on('someEvent2',function(msg){
+    setImmediate(function(){
+        console.log(msg)//emit中的参数
+    })  
+})
+
+myEmitter.emit('someEvent2','异步执行，我会在“2”先打印出来后，再打印出来')
+
+console.log(2)
